95e44b0327713efccfe991078b0670a8e607d616
Helen Chen <helen.weixu.chen@gmail.com>
2022-07-19 00:22:55 -0400

    Adding observer.h subject.h and TextDisplay.h files

---
+++ TextDisplay.h
@@ -0,0 +1,18 @@
+#ifndef TEXTDISPLAY_H
+#define TEXTDISPLAY_H
+#include <iostream>
+#include <vector>
+#include "observer.h"
+
+
+class Cell;
+
+class TextDisplay: public Observer<Info, State> {
+  std::vector<std::vector<char>> theDisplay;
+  const int gridSize;
+ public:
+  TextDisplay(int n);
+  void notify(Subject<Info, State> &whoNotified) override;
+  friend std::ostream &operator<<(std::ostream &out, const TextDisplay &td);
+};
+#endif
