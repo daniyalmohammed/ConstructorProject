9906c64d25c456381b92f2bf0668ce1761878aa7
Gio G <giovanighanem@hotmail.com>
2022-07-21 14:48:09 -0400

    Gio

--- builder.h
+++ builder.h
@@ -4,28 +4,29 @@
 #include <vector>
 
 class Builder {
-    std::string colour;// Blue/Red/Orange/Yellow
-    int playerNum; // player number
-    int buildingPoints; // points for the builder up to 10
-    int resourcesType[5];
-    // resources[0] = # brick;
-    // resources[1] = # energy;
-    // resources[2] = # glass;
-    // resources[3] = # heat;
-    // resources[4] = # wifi;
+    public:
+        std::string colour;// Blue/Red/Orange/Yellow
+        int playerNum; // player number
+        int buildingPoints; // points for the builder up to 10
+        int resourcesType[5];
+        // resources[0] = # brick;
+        // resources[1] = # energy;
+        // resources[2] = # glass;
+        // resources[3] = # heat;
+        // resources[4] = # wifi;
                
-    Builder(int playerNum); //initializes the builder and takes in the player number (0,1,2,3)
-    bool basementCanBuild(); // decide if there is enough resources for a basement
-    bool roadCanBuild(); // decide if there is enough resources for a road
-    bool towerCanBuild(); // decide if there is enough resources for a tower
-    void gooseMoving(int pos); 
-    void getInfo(); // call when a status of a builder is printed, output message
-    void loseResources(); //call on all builders when a 7 is rolled
-    //void improveResources(int type, int num);
-    //bool improveResidences(int pos, int residenceLevel);
-    //void steal(std::string current_builder, 
-    //std::string steal_from, int &amount, int &stolen_index);
-    // void storeStolen(int &amount, int &stolen_index);
+        Builder(int playerNum); //initializes the builder and takes in the player number (0,1,2,3)
+        bool basementCanBuild(); // decide if there is enough resources for a basement
+        bool roadCanBuild(); // decide if there is enough resources for a road
+        bool towerCanBuild(); // decide if there is enough resources for a tower
+        void gooseMoving(int pos); 
+        void getInfo(); // call when a status of a builder is printed, output message
+        void loseResources(); //call on all builders when a 7 is rolled
+        //void improveResources(int type, int num);
+        //bool improveResidences(int pos, int residenceLevel);
+        //void steal(std::string current_builder, 
+        //std::string steal_from, int &amount, int &stolen_index);
+        // void storeStolen(int &amount, int &stolen_index);
 
 };
 
