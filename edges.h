aefd711a4dc81b34d34d2f5a5dc033601217b6ad
Gio G <giovanighanem@hotmail.com>
2022-07-20 23:43:56 -0400

    Gio

--- edges.h
+++ edges.h
@@ -1,4 +1,19 @@
 #ifndef EDGES_H
 #define EDGES_H
 
+#include <vector>
+#include <string>
+#include "subject.h"
+
+class Edge : public Subject {
+    int pos;
+    int owner_index;
+    vector<int> neighborVertex;
+    vector<int> neighborEdges;
+    Edge(int location);
+    void colonize(int builder_index);
+    void notify();
+    string getEdgeInfo();
+};
+
 #endif
