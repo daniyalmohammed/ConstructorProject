786307c1139674c64abe6521def1498692d1e18d
Gio G <giovanighanem@hotmail.com>
2022-07-24 21:32:30 -0400

    Gio

--- board.cc
+++ board.cc
@@ -588,9 +588,16 @@ void Board::save(string file_name) {
 
     outfile << "0 3 1 10 3 5 1 4 5 7 3 10 2 11 0 3 3 8 0 2 0 6 1 8 4 12 1 5 4 11 2 4 4 6 2 9 2 9" << endl;
     outfile << goose_location << endl;
-    
+
 }
 
 
-
+void Board::getRez() {
+    for(auto v : vertices) {
+        if (v.owner_index == curTurn) {
+            cout << (v.pos) << " ";
+        } 
+    }
+    cout << endl;
+}
 
--- board.h
+++ board.h
@@ -63,5 +63,6 @@ class Board {
 	string GeeseStr(int index); // Done
 	void testTile(); // H -- done
 	void stealResource(int steal_from); // Dani -- done
+	void getRez();
 };
 #endif
